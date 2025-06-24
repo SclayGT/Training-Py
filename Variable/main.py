5edd390309f0615f3dc12eda0e7d80099cca8ae4
SclayGT <ronywijaya825@gmail.com>
2025-06-24 21:13:12 +0700

    Variable commit

--- Variable/main.py
+++ Variable/main.py
@@ -1,5 +1,19 @@
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
+# Variable adalah tempat penyimpanan data
+
+# Contoh deklarasi data
+# int a = 2
+
+# menaruh / Assignment data
+a = 10
+
+# Pemanggilan pertama
+print("Nilai a =",a)
+
+# Pemanggilan kedua
+a = 20
+print("Nilai a =",a)
+
+# Asignment indirect
+b = a
+print("Nilai b =",b)
+
