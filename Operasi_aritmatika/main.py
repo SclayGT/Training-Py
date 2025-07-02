b272778bf25998e2dd0bb05452ad863e7f1d6f57
SclayGT <ronywijaya825@gmail.com>
2025-07-02 21:19:07 +0700

    Operasi_aritmatika commit

---
+++ Operasi_aritmatika/main.py
@@ -0,0 +1,32 @@
+a = 11
+b = 3
+
+# Operasi perjumlahan
+print("a + b =", a + b)
+# Operasi pengurangan
+print("a - b =", a - b)
+# Operasi perkalian
+print("a * b =", a * b)
+# Operasi pembagian
+print("a / b =", a / b)
+# Operasi modulus
+print("a % b =", a % b)
+# Operasi pangkat
+print("a ** b =", a ** b)
+# Operasi floor division
+print("a // b =", a // b)
+
+print("==========================")
+
+# Prioritas operasi
+# 1. ()
+# 2. ** (pangkat)
+# 3. * / // %
+# 4. + -
+# Contoh
+print("a + b * 2 =", a + b * 2)
+print("a * b + 2 =", a * b + 2)
+print("(a + b) * 2 =", (a + b) * 2)
+print("a * (b + 2) =", a * (b + 2))
+
+
