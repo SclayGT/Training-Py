3ad362e75e860ff581d179c82f81b236e4051682
SclayGT <ronywijaya825@gmail.com>
2025-07-02 20:47:36 +0700

    Input_data commit

---
+++ Input_data/main.py
@@ -0,0 +1,20 @@
+# data Str
+data = input("Masukkan data : ")
+print("data = ",data)
+print("type data = ",type(data))
+
+# data Int
+data = int(input("Masukkan Angka : "))
+print("data = ",data)
+print("type data = ",type(data))
+
+# data Float
+data = float(input("Masukkan Angka : "))
+print("data = ",data)
+print("type data = ",type(data))
+
+# data Boolean
+data = bool( int(input("Masukkan Nilai Boolean : ")))
+print("data = ",data)
+print("type data = ",type(data))
+
